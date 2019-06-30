9c2db9b7d9b01cd34c620692e70a2fb416a92536
xie weiwei <xieweiwei201707@163.com>
2019-06-30 16:21:08 +0800

    feats: 动态规划中切绳子获取最大乘积

---
+++ src/dp/CuttingRope.java
@@ -0,0 +1,40 @@
+package dp;
+
+/**
+ * @Classname CuttingRope
+ * @Description TODO
+ * @Date 2019/6/30 15:34
+ * @Created by xww
+ */
+public class CuttingRope {
+
+    public static int maxCutting(int length){
+        if(length<2) return 0;
+        if(length==2)return 1;
+        if(length==3)return 2;
+        int[] dp = new int[length+1];
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=2;
+        dp[3]=3;
+        int max = 0;
+        int temp = 0;
+        for(int i=4;i<=length;i++){
+            max = 0;
+            for(int j=1;j<=i/2;j++){
+                temp = dp[j]*dp[i-j];
+                if(temp>max)
+                    max = temp;
+            }
+            dp[i] = max;
+        }
+        return dp[length];
+    }
+
+    public static void main(String[] args){
+        for(int i=2;i<10;i++){
+            System.out.println("长度为"+i+"的最大值->"+maxCutting(i));
+        }
+    }
+}
+
